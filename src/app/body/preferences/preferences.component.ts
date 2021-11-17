321ecf0034ef1d4ac43e46b4250861e077d844e7
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-11-17 20:32:53 +0100

    Preferences component

---
+++ src/app/body/preferences/preferences.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-preferences',
+  templateUrl: './preferences.component.html',
+  styleUrls: ['./preferences.component.css'],
+})
+export class PreferencesComponent {
+  cardsNumberList: number[] = [20, 26, 32];
+  timeLimitList: number[] = [0, 60, 90, 120, 150];
+  preferencesForm = new FormGroup({
+    cardsNumber: new FormControl(this.cardsNumberList[0], [
+      Validators.required,
+    ]),
+    timeLimit: new FormControl(this.timeLimitList[0], [Validators.required]),
+  });
+
+  constructor(private router: Router) {}
+
+  savePreferences() {
+    const cardsNumber = this.preferencesForm.get('cardsNumber')?.value;
+    const timeLimit = this.preferencesForm.get('timeLimit')?.value;
+    if (this.preferencesForm.valid) {
+      localStorage.setItem('cards-number', cardsNumber);
+      localStorage.setItem('time-limit', timeLimit);
+    }
+    this.router.navigateByUrl('/play');
+  }
+}
